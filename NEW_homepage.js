18f251db5f3469d7738ce6ba249f252ff0d73220
Thanyani-py <orineamulaudzi2@gmail.com>
2024-10-09 18:37:36 +0200

    updated

---
+++ NEW_homepage.js
@@ -0,0 +1,7 @@
+document.getElementById("customerLink").addEventListener("click", function() {
+    alert("You clicked on Customer!");
+});
+
+document.getElementById("staffLink").addEventListener("click", function() {
+    alert("You clicked on Staff!");
+});
